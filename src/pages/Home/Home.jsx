47fd835b6caad261e7f55b73ae4812c880f940aa
Ajarek <ajarek@poczta.onet.pl>
2023-08-01 00:47:24 +0200

    added css component Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,20 @@
+import { Link } from 'react-router-dom'
 import './Home.css'
 
 const Home = () => {
-
-  
   return (
     <div className='home'>
-      Home
-     
+      <div className='home-info'>
+        <h1>NAUCZ SIĘ JEŹDZIĆ W 3 MIESIĄCE</h1>
+        <p>
+          Naucz się jeździć w 3 miesiące. Już 15 lat zapewniamy wysokiej jakości lekcje
+          jazdy , które kończy ponad 150 kursantów miesięcznie.
+        </p>
+        <Link className='home-info-link'>Zapisz się →</Link>
+      </div>
+      <div className="home-img">
+
+      </div>
     </div>
   )
 }
